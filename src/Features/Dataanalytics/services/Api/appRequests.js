98d5125ad135308bc08c179adbbef61c85e806bf
vaibhav-sharma-oss <80534369+iamvaibhav31@users.noreply.github.com>
2023-03-31 15:18:26 +0530

    Fixed the Htttp error

--- src/Features/Dataanalytics/services/Api/appRequests.js
+++ src/Features/Dataanalytics/services/Api/appRequests.js
@@ -5,13 +5,11 @@ const instance = axios.create({
   baseURL: "http://go-dev.greedygame.com/v3/dummy/apps",
 });
 
-instance.defaults.baseURL = "https://go-dev.greedygame.com/v3/dummy/apps";
+instance.defaults.baseURL = "https://go-dev.greedygame.com/v3/dummy";
 
 export const Appnames = createAsyncThunk("App/Names", async () => {
   try {
-    const response = await instance.get(
-      "http://go-dev.greedygame.com/v3/dummy/apps",
-    );
+    const response = await instance.get("/apps");
     return response.data.data;
   } catch (e) {
     return e.message;
@@ -20,8 +18,8 @@ export const Appnames = createAsyncThunk("App/Names", async () => {
 
 export const Appdata = createAsyncThunk("App/Data", async (date) => {
   try {
-    const response = await axios.get(
-      `https://go-dev.greedygame.com/v3/dummy/report?startDate=${date.From}&endDate=${date.To}`,
+    const response = await instance.get(
+      `/report?startDate=${date.From}&endDate=${date.To}`,
     );
     return response.data.data;
   } catch (e) {
