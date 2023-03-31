59c4d538601238dc1ec7ee8eae90733c17728833
vaibhav-sharma-oss <80534369+iamvaibhav31@users.noreply.github.com>
2023-03-31 15:15:11 +0530

    Fixed the Htttp error

--- src/Features/Dataanalytics/services/Api/appRequests.js
+++ src/Features/Dataanalytics/services/Api/appRequests.js
@@ -1,20 +1,30 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "http://go-dev.greedygame.com/v3/dummy/apps",
+});
+
+instance.defaults.baseURL = "https://go-dev.greedygame.com/v3/dummy/apps";
+
 export const Appnames = createAsyncThunk("App/Names", async () => {
-          try {
-                    const response = await axios.get("http://go-dev.greedygame.com/v3/dummy/apps")
-                    return response.data.data
-          } catch (e) {
-                    return e.message
-          }
-})
+  try {
+    const response = await instance.get(
+      "http://go-dev.greedygame.com/v3/dummy/apps",
+    );
+    return response.data.data;
+  } catch (e) {
+    return e.message;
+  }
+});
 
 export const Appdata = createAsyncThunk("App/Data", async (date) => {
-          try {
-                    const response = await axios.get(`https://go-dev.greedygame.com/v3/dummy/report?startDate=${date.From}&endDate=${date.To}`)
-                    return response.data.data
-          } catch (e) {
-                    return e.message
-          }
-})
+  try {
+    const response = await axios.get(
+      `https://go-dev.greedygame.com/v3/dummy/report?startDate=${date.From}&endDate=${date.To}`,
+    );
+    return response.data.data;
+  } catch (e) {
+    return e.message;
+  }
+});
